38147dd42b5b794b6e0c4f7863f6739865b019de
karnamsneha23-sketch <karnamsneha23@gmail.com>
2026-04-23 00:30:26 +0530

    Update text_to_sign.py

--- text_to_sign.py
+++ text_to_sign.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import os
 
-st.title("Text to Sign")
+def text_to_sign_page():
+    st.title("Text to Sign")
 
-text = st.text_input("Enter text")
+    text = st.text_input("Enter text")
 
-if text:
-    for ch in text.lower():
-        file_name = f"{ch.upper()}.jpeg"   # matches your files
+    if text:
+        for ch in text.lower():
+            file_name = f"{ch.upper()}.jpeg"
 
-        if os.path.exists(file_name):
-            st.image(file_name, width=100)
-        else:
-            st.write(f"{ch} ❌ not found")
+            if os.path.exists(file_name):
+                st.image(file_name, width=100)
+            else:
+                st.write(f"{ch} ❌ not found")
